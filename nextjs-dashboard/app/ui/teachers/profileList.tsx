a7e1dfa6943ae598c7a0718073cf49a79978073c
Arup Sankar Reang <xocionrocks247@gmail.com>
2024-03-30 10:56:48 +0530

    featured teachers 3

--- nextjs-dashboard/app/ui/teachers/profileList.tsx
+++ nextjs-dashboard/app/ui/teachers/profileList.tsx
@@ -4,9 +4,9 @@ import {profiles} from '@/app/pageData'
 
 const ProfileList = () => {
   return (
-    <div className='flex  overflow-x-auto overflow-y-hidden w-screen gap-2 md:overflow-y-auto md:flex-col md:w-60 md:h-[85vh] rounded-2xl'>
+    <div className='flex  overflow-x-auto overflow-y-hidden w-screen gap-5 md:overflow-y-auto md:flex-col md:w-64 md:h-[85vh] '>
       {profiles.map((profile, index) => (
-        <div key={index} className='flex-shrink-0  bg-slate-50 shadow-xl rounded-3xl hover:shadow-2xl'>
+        <div key={index} className='flex-shrink-0  bg-slate-50 shadow-xl rounded-3xl hover:shadow-2xl '>
           <ProfileCard  {...profile} />
         </div>
       ))}
