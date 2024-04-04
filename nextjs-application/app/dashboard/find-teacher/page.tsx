0b03bcdb91c395f120244d4aa30dc4d699fdac9a
Deeptanu-Dhar <deepdhar892@gmail.com>
2024-04-04 16:27:31 +0530

    find teacher UI designed

--- nextjs-application/app/dashboard/find-teacher/page.tsx
+++ nextjs-application/app/dashboard/find-teacher/page.tsx
@@ -1,87 +1,100 @@
-"use client"
+'use client';
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 // import { fetchTeachers } from '@/app/lib/actions';
 import { handleAxiosError } from '@/app/lib/handleAxiosError';
 import { Teacher } from './responseType';
+import Image from 'next/image';
+import Link from 'next/link';
+import { PlusCircleIcon } from '@heroicons/react/24/outline';
+import { Button } from '@/app/ui/button';
 
 export default function Page() {
- const [searchQuery, setSearchQuery] = useState<string>('');
- const [searchResults, setSearchResults] = useState<Teacher[]>([]);
- const [loading, setLoading] = useState<boolean>(false);
- const [allTeachers, setAllTeachers] = useState<Teacher[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<Teacher[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [allTeachers, setAllTeachers] = useState<Teacher[]>([]);
+  const profileImg = '/default_profile.svg';
 
+  useEffect(() => {
+    async function fetchTeachers() {
+      setLoading(true);
+      const apiEndpoint =
+        'https://my-classes-backend.onrender.com/api/v1/student/showTeachers';
 
- useEffect(() => {
-
-  async function fetchTeachers(){
-    setLoading(true);
-    const apiEndpoint = 'https://my-classes-backend.onrender.com/api/v1/student/showTeachers';
-  
-    try{
-      const response = await axios.get(apiEndpoint)
-      console.log('the data')
-      console.log(response.data.data);
-      setAllTeachers(response.data.data)
-    }catch (error){
-      const message = handleAxiosError(error);
-      console.log(message)
-    }finally{
-      setLoading(false);
+      try {
+        const response = await axios.get(apiEndpoint);
+        console.log('the data');
+        console.log(response.data.data);
+        setAllTeachers(response.data.data);
+      } catch (error) {
+        const message = handleAxiosError(error);
+        console.log(message);
+      } finally {
+        setLoading(false);
+      }
     }
-  };
-    
 
-  fetchTeachers();
- }, []);
+    fetchTeachers();
+  }, []);
 
-
-
- return (
-    <main className="flex flex-col bg-gray-200 rounded-xl p-3">
+  return (
+    <main className="flex flex-col rounded-xl bg-gray-200 p-3">
       <div>
         {/* <form onSubmit={handleSearch} className='flex'> */}
-        <form  className='flex' action='fetchTeachers()'>
+        <form className="flex" action="fetchTeachers()">
           <input
             type="text"
-            className='w-10/12 rounded-3xl border-0 border-b-fuchsia-900'
+            className="w-10/12 rounded-3xl border-0 border-b-fuchsia-900"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="Search for teachers..."
           />
-          <button type="submit" className='bg-violet-700 w-2/12 h-8 text-white self-center mx-1 rounded-full'>Search</button>
+          <button
+            type="submit"
+            className="mx-1 h-8 w-2/12 self-center rounded-full bg-violet-700 text-white"
+          >
+            Search
+          </button>
         </form>
       </div>
-      
 
-      <div className="flex flex-col bg-yellow-200 my-3">
-        <strong>List of teachers</strong>
+      <h1 className="m-4 text-center text-4xl font-bold">Our Teachers</h1>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
         {loading ? (
           <p>Loading...</p>
         ) : (
-
-
           allTeachers.map((teacher, index) => (
-            <div key={index} className='bg-violet-300 rounded-lg p-3 m-3'>
-              <h2>{teacher.description}</h2>
-              <p>Locality: {teacher.locality}</p>
-              <p>Complete Address: {teacher.completeAddress}</p>
-              <p>Status: {teacher.status}</p>
-              <h3>Teacher Information:</h3>
-              <p>Name: {teacher.TeacherInfo.name}</p>
-              <p>Email: {teacher.TeacherInfo.email}</p>
-              <p>Phone Number: {teacher.TeacherInfo.phoneNumber}</p>
-            </div>
+            <Link href="">
+              <div
+                key={index}
+                className=" hover: m-4 flex items-center justify-between rounded-lg bg-gradient-to-br from-slate-100 to-slate-200 p-4 shadow-lg hover:shadow-xl transition-all ease-linear"
+              >
+                <Image
+                  height={70}
+                  width={70}
+                  src={profileImg}
+                  alt="pfp"
+                  className=""
+                />
+                <div className="flex flex-1 flex-col text-center">
+                  <h2 className="text-2xl font-bold md:text-xl">
+                    {teacher.TeacherInfo.name}
+                  </h2>
+                  <p>{teacher.subjectTeaching}</p>
+                </div>
+                <div>
+                  <button>
+                    <PlusCircleIcon className='w-10 text-violet-700 hover:text-violet-400 ease-linear transition-colors md:w-7'/>
+                  </button>
+                </div>
+              </div>
+            </Link>
           ))
-
-
         )}
-
       </div>
-      <div>
-        {/* page numbers */}
-      </div>
+      <div>{/* page numbers */}</div>
     </main>
- );
+  );
 }
