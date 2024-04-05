ca864ec3c6ac33a5c9bde9d0d5059cdea93dbaf4
Arup Sankar Reang <xocionrocks247@gmail.com>
2024-04-05 12:08:46 +0530

    profile page ui

--- nextjs-application/app/dashboard/profile/page.tsx
+++ nextjs-application/app/dashboard/profile/page.tsx
@@ -1,36 +1,3 @@
-// "use client"
-// import Link from 'next/link';
-// import React, { useState, useEffect } from "react";
-// import { userdata } from "@/app/lib/actions";
-// import { Session } from "next-auth";
-
-// export default function ProfilePage() {
-
-//     const [data, setData] = useState<Session | null>(null);
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             const result = await userdata();
-//             setData(result);
-//         };
-
-//         fetchData()
-//     }, [])
-
-//     return (
-//         <main>
-//             <div className="flex h-full flex-col  pb-4 ">
-//                 <span> here is the user profile: </span><br></br>
-//                 <span>{data?.user?.name}</span><br></br>
-//                 <span>{data?.user?.email}</span><br></br>
-//                 <span>{data?.user?.image}</span><br></br>
-//                 <Link
-//                     className="mb-2 flex  items-end justify-start  bg-violet-600  md:h-40"
-//                     href="/dashboard"
-//                 >
-//             </div>
-//         </main>
-//     );
-// }
 "use client"
 import Link from 'next/link';
 import React, { useState, useEffect } from "react";
@@ -51,6 +18,16 @@ export default function ProfilePage(){
         fetchData();
     }, []);
 
+    const contentlist= [
+    {name:'dashboard', href:''},
+    {name:'Payment', href:''},
+    {name:'Teachers & Mentors', href:''},
+    {name:'Settings', href:''},
+    {name:'Help & feedback', href:''},
+    {name:'About', href:''},
+    {name:'T&C', href:''},
+    ];
+
     return (
         <main>
 
@@ -66,42 +43,18 @@ export default function ProfilePage(){
                     <a className='cursor-pointer'href=''>View Profile &gt;</a>
                     
                 </div>
-                <div className='container '>
-                    <Link legacyBehavior href="/dashboard">
-                        <div className=''>
-                            <a className="text=center rounded-full  pl-10 pt-5 pb-4 mb-2 w-full flex  items-end justify-start  bg-blue-400">Dashboard</a>
-                        </div>
-                    </Link>
-                    <Link legacyBehavior href="">
-                        <div className=''>
-                            <a className="cursor-default rounded-full pl-10 pt-5 pb-4 mb-2 w-full flex  items-end justify-start  bg-blue-400">Payment</a>
-                        </div>
-                    </Link>
-                    <Link legacyBehavior href="">
-                        <div className=''>
-                            <a className="cursor-default rounded-full pl-10 pt-5 pb-4 mb-2 w-full flex  items-end justify-start  bg-blue-400">Teachers & Mentors</a>
-                        </div>
-                    </Link>
-                    <Link legacyBehavior href="">
-                        <div className=''>
-                            <a className="cursor-default rounded-full pl-10 pt-5 pb-4 mb-2 w-full flex  items-end justify-start  bg-blue-400">Settings</a>
-                        </div>
-                    </Link>
-                    <Link legacyBehavior href="">
-                        <div className=''>
-                            <a className="cursor-default rounded-full pl-10 pt-5 pb-4 mb-2 w-full flex  items-end justify-start  bg-blue-400">Help & feedback</a>
-                        </div>
-                    </Link>
-                    <Link legacyBehavior href="">
-                        <div className=''>
-                            <a className="cursor-default rounded-full pl-10 pt-5 pb-4 mb-2 w-full flex  items-end justify-start  bg-blue-400">About</a>
-                        </div>
-                    </Link>
-                    <Link legacyBehavior href="">
-                        <div className=''>
-                            <a className="cursor-default rounded-full pl-10 pt-5 pb-4 mb-2 w-full flex  items-end justify-start  bg-blue-400">T&C</a>
-                        </div>
-                    </Link>
+                <div  className='flex flex-col gap-2'>
+                    {contentlist.map((cont , index) =>(
+                        <Link  href={cont.href} key={index} 
+                        className="flex justify-center rounded-md h-16 bg-violet-500 mx-4 hover:bg-violet-300">
+            
+                                <span className='font-bold text-2xl flex self-center'>
+                                {cont.name}
+                                </span>
+                        </Link>
+                    ))}
+                    
+                    
                 </div>
 
             </div>
