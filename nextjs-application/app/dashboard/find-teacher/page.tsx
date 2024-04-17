6bfd25b821f864486a873634fff14c361ba967b2
Arup Sankar Reang <xocionrocks247@gmail.com>
2024-04-17 11:37:27 +0530

    teacher profile ui changes

--- nextjs-application/app/dashboard/find-teacher/page.tsx
+++ nextjs-application/app/dashboard/find-teacher/page.tsx
@@ -6,23 +6,11 @@ import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ArrowRightCircleIcon } from '@heroicons/react/24/outline';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-
+import {  Select,  SelectContent,  SelectGroup,  SelectItem,  SelectLabel,  SelectTrigger,  SelectValue,
 } from "./ui/select"
-import FindProfileCard from './ui/find-profileCard';
 import { Input } from "./ui/input"
-
-import dotenv from 'dotenv'
 import { useStore } from './ui/Management';
-import { backendFetch} from '@/app/lib/actions';
-
+import { backendFetch } from '@/app/lib/actions';
 
 export default function Page() {
   const [loading, setLoading] = useState<boolean>(false);
@@ -36,84 +24,74 @@ export default function Page() {
       setLoading(true);
       const backendurl = await backendFetch()
       const apiEndpoint = `${backendurl}/student/showTeachers`;
-      
+      console.log(apiEndpoint)
 
       try {
         const response = await axios.get(apiEndpoint);
-      await addTeacher(response.data.data)
-      await changeAlradyFetchedTeacher()
-    } catch (error) {
-    } finally {
+        addTeacher(response.data.data)
+        changeAlradyFetchedTeacher()
+      } catch (error) {
+      
+      } finally {
       setLoading(false);
     }
-  }
+    }
   fetchTeachers();
   ++value
-});
+}, []);
 
 
   return (
     <main className="flex flex-col rounded-xl bg-gray-200 p-3">
       
-
       <h1 className="m-4 text-center text-4xl font-bold">Our Teachers</h1>
       <div className='flex items-center justify-around sm:gap-20  gap-4 px-4' >
-       <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Experience</SelectItem>
-          <SelectItem value="banana">Subject</SelectItem>
-          <SelectItem value="blueberry">Rating</SelectItem>
-          
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-<Input />
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="categories" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            
+            <SelectItem value="apple">Science</SelectItem>
+            <SelectItem value="banana">Biology</SelectItem>
+            <SelectItem value="blueberry">Geography</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Input id="input" className="truncate rounded-full border-0" type="search"  
+       aria-controls="matches" placeholder="search" aria-expanded="false" aria-live="polite"/>
+        
           </div>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
+      <div className="flex flex-col lg:flex-row gap-2 flex-wrap justify-evenly my-6">
         {loading ? (
           <p>Loading...</p>
         ) : (
           <>
-        
 
           {
           Teachers.map((teacher, index) => (
-            <div
-              key={index}
-              className=" hover: ease-linea m-4 flex items-center justify-between rounded-lg bg-gradient-to-br from-slate-100 to-slate-200 p-4 shadow-lg transition-all hover:shadow-xl"
+            <Link href={`/dashboard/teacher-profile-page/${teacher.teacherId}`} key={index}
+            className=" transition-all ease-in hover:scale-105 flex  items-center  rounded-xl
+            border-gray-950/20 border-solid border-2 
+            bg-gradient-to-br from-slate-50 via-slate-50 to-indigo-700/20
+             shadow-md hover:shadow-2xl lg:w-[48%] justify-evenly p-2 md:py-6 m-1  "
             >
-              <Image
-                height={70}
-                width={70}
-                src={profileImg}
-                alt="pfp"
-                className="w-12 "
+              <Image height={70} width={70} src={profileImg}
+                alt="profile picture" className="w-24 sm:w-36 md:w-44  "
               />
-              <div className="flex flex-1 flex-col text-center">
-                <h2 className="px-2 text-base font-bold md:text-xl">
-                  {teacher.Teachers.name}
-                </h2>
-                <p>{teacher.subjectTeaching}</p>
-              </div>
-              <div>
-                <Link href={`/dashboard/teacher-profile-page/${teacher.teacherId}`}>
-                  <button>
-                    <ArrowRightCircleIcon className="w-10 text-violet-700 transition-all ease-linear hover:text-violet-400 md:w-7 hover:translate-x-0.5" />
-                  </button>
-                </Link>
-              </div>
-            </div>
+                <div className='flex flex-col w-1/2'>
+                  <h2 className="  font-semibold text-base sm:text-xl">
+                    {teacher.Teachers.name}
+                  </h2>
+                  <span className='text-base sm:text-xl'>{teacher.description}</span>
+                  <span className='text-base sm:text-xl'>{teacher.subjectTeaching}</span>
+                  <span className='text-base sm:text-xl'>{teacher.qualification}</span>
+                </div>
+            </Link>
           ))}
           </>
-        )
-        
-        }
-
+        )}
       </div>
     </main>
   );
--- nextjs-application/app/ui/services.tsx
+++ nextjs-application/app/ui/services.tsx
@@ -18,7 +18,7 @@ export default function Services(){
             return(
 
               <Link key={item.name} href={item.href} className=' hover:shadow-2xl relative flex flex-col 
-              shadow-lg rounded-3xl h-auto transition-all ease-in items-center p-1 hover:scale-105' >
+              shadow-lg rounded-3xl h-auto  items-center p-1 transition-all ease-in hover:scale-105' >
               <Image src={item.imagesrc}  width={100} height={100} className='w-36 h-40 mx-3' alt={item.name} ></Image>
               <span className='font-bold text-sm p-2 text-center'>{item.name}
               </span>
