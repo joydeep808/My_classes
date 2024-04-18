9d8ccb13abbf86674a2e5aec02358b622bbe93cf
Deeptanu-Dhar <deepdhar892@gmail.com>
2024-04-18 20:31:28 +0530

    fixed layout so that footer goes to bottom

--- nextjs-application/app/dashboard/layout.tsx
+++ nextjs-application/app/dashboard/layout.tsx
@@ -1,15 +1,16 @@
 import SideNav from '@/app/dashboard/ui/sidenav';
 import Footer from '../ui/footer';
 import ScrollToTop from '../ui/scrollToTop';
- 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col ">
-      <SideNav /> 
+    <div className="flex flex-col h-screen">
+      <SideNav />
       <div className="flex-grow   ">
-        <ScrollToTop/>
-        {children}</div>
-      <Footer/>
+        <ScrollToTop />
+        {children}
+      </div>
+      <Footer />
     </div>
   );
 }
