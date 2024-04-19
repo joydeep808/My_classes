67457fbdb8e26e1e9e984047b2ed70e35689c411
Arup Sankar Reang <xocionrocks247@gmail.com>
2024-04-19 17:43:02 +0530

    idk

--- nextjs-application/app/ui/teachers/profileList.tsx
+++ nextjs-application/app/ui/teachers/profileList.tsx
@@ -1,34 +1,34 @@
+import React from 'react';
 import ProfileCard from './profilecard';
-import {profiles} from '@/app/pageData'
+import { profiles } from '@/app/pageData';
 import Marquee from "react-fast-marquee";
 
-const viewportx = window.innerWidth;
-const setgradient:boolean = viewportx>=1024;
-
 const ProfileList = () => {
-  return (
+  // Check if window is defined before accessing its properties
+  const viewportx = typeof window !== 'undefined' ? window.innerWidth : 0;
+  const setgradient = viewportx >= 1024;
 
-    <div className="flex flex-col items-center self-start justify-center  md:h-2/5   md:mb-auto mt-4 ">
+  return (
+    <div className="flex flex-col items-center self-start justify-center md:h-2/5 md:mb-auto mt-4">
       <strong className='font-extrabold text-3xl md:text-4xl m-4'>
-        Top Rated Teachers</strong>
+        Top Rated Teachers
+      </strong>
 
       <div className='flex w-screen bg-gradient-to-t from-indigo-600'>
-        <Marquee 
+        <Marquee
           pauseOnHover={true}
           pauseOnClick={true}
-          className=''
           gradient={setgradient}
+          className=''
         >
-          
           {profiles.map((profile, index) => (
-            <div key={index} className='flex-shrink-0  bg-slate-50 shadow-xl rounded-3xl hover:shadow-2xl my-5
-            transition-all hover:scale-105 ease-in mx-2'>
-              <ProfileCard  {...profile} />
+            <div key={index} className='flex-shrink-0 bg-slate-50 shadow-xl rounded-3xl hover:shadow-2xl my-5 transition-all hover:scale-105 ease-in mx-2'>
+              <ProfileCard {...profile} />
             </div>
           ))}
         </Marquee>
       </div>
-    </div> 
+    </div>
   );
 };
 
